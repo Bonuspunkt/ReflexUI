707ac89c5274a08bf3d8deb929c23f2b4c0a3609
Bonuspunkt <bonuspunkt+github@gmail.com>
2015-04-11 03:34:11 +0200

    make WidgetList toggleable with cl_crosshair hack

--- WidgetList.lua
+++ WidgetList.lua
@@ -1,11 +1,33 @@
 local FuncArray = require "base/internal/ui/bonus/_FuncArray"
+local Color = require "base/internal/ui/bonus/_Color"
+
+local crosshair;
+local shouldDraw = false;
 
 WidgetList =
 {
+    canHide = false,
+    canPosition = false,
+
     draw = function()
 
+        local currentCrosshair = consoleGetVariable('cl_crosshair')
+
+        if crosshair == nil then
+            crosshair = currentCrosshair
+        end
+        if (currentCrosshair == 666) then
+            shouldDraw = not shouldDraw
+            consolePerformCommand('cl_crosshair ' .. crosshair)
+        end
+
+        if not shouldDraw then
+            if #players ~= 0 then
+                return
+            end
+        end
+
         nvgFontFace("TitilliumWeb-Bold");
-        nvgFontSize(32)
         nvgTextAlign(NVG_ALIGN_LEFT, NVG_ALIGN_TOP);
 
         local widgets = {}
@@ -16,6 +38,15 @@ WidgetList =
             end
         end
 
+        local offsetX = - viewport.width / 2 + 250;
+        local offsetY = - viewport.height / 2 + 100;
+
+        nvgFontSize(36)
+        nvgFillColor(Color(255,0,0))
+        nvgText(offsetX, offsetY, "AVAILABLE WIDGETS")
+
+        nvgFontSize(32)
+        nvgFillColor(Color(255,255,255))
         FuncArray(widgets)
             .filter(function(w)
                 return w.canHide ~= false
@@ -27,9 +58,11 @@ WidgetList =
             .forEach(function(w, i)
                 -- info about canPosition
 
-                nvgText(300, 30*i, w.name)
+                nvgText(offsetX, offsetY + 30*i, w.name)
             end)
 
     end
 };
 registerWidget("WidgetList");
+
+--consolePerformCommand("bind i cl_crosshair 666")
