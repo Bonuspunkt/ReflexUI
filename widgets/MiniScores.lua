824702d3d0a135123707c14819ff6a1d5a218bee
Bonuspunkt <bonuspunkt+github@gmail.com>
2015-04-18 23:31:20 +0200

    add more options

--- widgets/MiniScores.lua
+++ widgets/MiniScores.lua
@@ -2,21 +2,43 @@ require "base/internal/ui/reflexcore"
 local const = require "../const"
 local nvg = require "../nvg"
 local ui = require "../ui"
+local userData = require "../userData"
 local color = require "../lib/color"
 local funcArray = require "../lib/funcArray"
 
+local widgetName = "MiniScores"
+local ORIENTATION_HORZ = "Horizontal"
+local ORIENTATION_VERT = "Vertical"
+local orientations = { ORIENTATION_HORZ, ORIENTATION_VERT }
+local orientationState = {}
 local config
 
 local function initOrFixConfig()
-  if not config then config = _G.loadUserData() or {} end
+  if not config then config = userData.load() or {} end
 
   if not config.ourColor then config.ourColor = color.new(64,64,255,64) end
   if not config.diffColor then config.diffColor = color.new(255,255,255,64) end
   if not config.theirColor then config.theirColor = color.new(255,64,64,64) end
+  if not config.orientation then config.orientation = ORIENTATION_HORZ end
+end
+
+local function drawColorSliders(x, y, label, table, property)
+
+  ui.label(label, x, y+30);
+
+  table[property].r = ui.slider(x + 120, y+0,  255, 0, 255, table[property].r)
+  table[property].g = ui.slider(x + 120, y+20, 255, 0, 255, table[property].g)
+  table[property].b = ui.slider(x + 120, y+40, 255, 0, 255, table[property].b)
+  table[property].a = ui.slider(x + 120, y+60, 255, 0, 255, table[property].a)
+
+  nvg.beginPath()
+  nvg.rect(x + 400, y, 100, 90)
+  nvg.fillColor(table[property])
+  nvg.fill()
 end
 
 
-_G.MiniScores = {
+local MiniScores = {
 
   draw = function()
 
@@ -31,7 +53,7 @@ _G.MiniScores = {
     local textColor = color.new(255,255,255)
 
     nvg.fontFace(const.font.textBold)
-    nvg.textAlign(nvg.const.hAlign.center, nvg.const.vAlign.top)
+    nvg.textAlign(nvg.const.hAlign.center, nvg.const.vAlign.middle)
 
     local activePlayers = funcArray(_G.players).filter(
       function(p)
@@ -69,96 +91,79 @@ _G.MiniScores = {
         .reduce(function(prev, curr) return math.max(prev, curr) end, 0)
     end
 
-
+    -- calculate positions
     nvg.fontSize(64);
-    local width = 80;
+    local size = nvg.textBounds("-888")
+    local padding = 5
+    local width = (size.maxx - size.minx) + 2 * padding
+    local height = (size.maxy - size.miny) -- height is way off so no need for padding XD
 
     local count = config.showDiff and 3 or 2
     local x = -width * count / 2
+    local y = 0
 
-    -- our score
-    nvg.beginPath();
-    nvg.rect(x, 5, width, 55);
-    nvg.fillColor(config.ourColor);
-    nvg.fill();
+    -- some helpers
+    local function step()
+      if (config.orientation == ORIENTATION_HORZ) then
+        x = x + width
+      else
+        y = y + height
+      end
+    end
 
-    nvg.fillColor(textColor);
-    nvg.text(x + width/2, 0, ourScore);
-
-    x = x + width
-
-    -- difference
-    if config.showDiff then
+    local function drawScore(color, text)
       nvg.beginPath();
-      nvg.rect(x, 5, 80, 55);
-      nvg.fillColor(config.diffColor);
+      nvg.rect(x, y, width, height);
+      nvg.fillColor(color);
       nvg.fill();
 
       nvg.fillColor(textColor);
-      nvg.text(x + width / 2,0, ourScore - theirScore);
+      nvg.text(x + width/2, y + height / 2, text);
+    end
 
-      x = x + width
+    -- our score
+    drawScore(config.ourColor, ourScore)
+    step()
+
+    -- difference
+    if config.showDiff then
+      drawScore(config.diffColor, ourScore - theirScore)
+      step()
     end
 
     -- their score
-    nvg.beginPath();
-    nvg.rect(x, 5, width, 55);
-    nvg.fillColor(config.theirColor);
-    nvg.fill();
-
-    nvg.fillColor(textColor);
-    nvg.text(x + width / 2,0, theirScore);
+    drawScore(config.theirColor, theirScore)
+    step()
   end,
 
   drawOptions = function(_, x, y)
     initOrFixConfig()
 
-    ui.label("Your Score:", x, y+30);
+    ui.label("Orientation", x, y)
+    local comboX = x + 120
+    local comboY = y
+    y = y + 40
 
-    config.ourColor.r = ui.slider(x + 120, y, 255, 0, 255, config.ourColor.r)
-    config.ourColor.g = ui.slider(x + 120, y+20, 255, 0, 255, config.ourColor.g)
-    config.ourColor.b = ui.slider(x + 120, y+40, 255, 0, 255, config.ourColor.b)
-    config.ourColor.a = ui.slider(x + 120, y+60, 255, 0, 255, config.ourColor.a)
+    drawColorSliders(x, y, "Your Score", config, "ourColor")
 
-    nvg.beginPath()
-    nvg.rect(x + 400, y, 100, 90)
-    nvg.fillColor(config.ourColor)
-    nvg.fill()
-
+    -- we draw he comboBox after the sliders, because the comboBox dropdown
+    -- must be drawn over the sliders
+    config.orientation = ui.comboBox(orientations, config.orientation, comboX, comboY, 255, orientationState)
     y = y + 90
 
     config.showDiff = ui.checkBox(config.showDiff or false, "Show Diff", x, y)
     y = y + 30
 
     if config.showDiff then
-      ui.label("Score diff:", x, y+30);
-
-      config.diffColor.r = ui.slider(x + 120, y, 255, 0, 255, config.diffColor.r)
-      config.diffColor.g = ui.slider(x + 120, y+20, 255, 0, 255, config.diffColor.g)
-      config.diffColor.b = ui.slider(x + 120, y+40, 255, 0, 255, config.diffColor.b)
-      config.diffColor.a = ui.slider(x + 120, y+60, 255, 0, 255, config.diffColor.a)
-
-      nvg.beginPath()
-      nvg.rect(x + 400, y, 100, 90)
-      nvg.fillColor(config.diffColor)
-      nvg.fill()
-
+      drawColorSliders(x, y, "Score Diff", config, "diffColor")
       y = y + 90
     end
 
-    ui.label("Other score:", x, y+30);
+    drawColorSliders(x, y, "Other Score", config, "theirColor")
 
-    config.theirColor.r = ui.slider(x + 120, y, 255, 0, 255, config.theirColor.r)
-    config.theirColor.g = ui.slider(x + 120, y+20, 255, 0, 255, config.theirColor.g)
-    config.theirColor.b = ui.slider(x + 120, y+40, 255, 0, 255, config.theirColor.b)
-    config.theirColor.a = ui.slider(x + 120, y+60, 255, 0, 255, config.theirColor.a)
-
-    nvg.beginPath()
-    nvg.rect(x + 400, y, 100, 90)
-    nvg.fillColor(config.theirColor)
-    nvg.fill()
-
-    _G.saveUserData(config);
+    userData.save(config);
   end
 };
-_G.registerWidget("MiniScores");
+
+_G[widgetName] = MiniScores;
+_G.registerWidget(widgetName);
